5921c906e81bda2ebc04e6bf0762e5f382b49710
Subham <subham.sinha41@gmail.com>
2021-04-03 15:26:47 +0530

    new commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StateComponent } from './state/state.component';
+import { CountriesComponent } from './countries/countries.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ declarations: [
+   AppComponent,
+   StateComponent,
+   CountriesComponent
+ ],
+ imports: [
+   BrowserModule,
+   HttpClientModule
+ ],
+ providers: [HttpClientModule],
+ bootstrap: [AppComponent]
 })
 export class AppModule { }
